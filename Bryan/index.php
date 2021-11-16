9deb9a42d2400045e32ef03a08b0cebcae613257
Arthur57330 <arthur.trus1@gmail.com>
2021-11-16 22:34:00 +0100

    g

--- Bryan/index.php
+++ Bryan/index.php
@@ -121,7 +121,7 @@
     <div class="millieu">
         <h1>Les Promotions du moment !!</h1>
         <div class="promo">
-        <div class="toutpromo">
+        <div class="toutpromo1">
             <div class="item_promo">
         
                 <div class="image_produit"><img src="/image/cigarette-electronique-80w-complet-kit-kit-vape-av.jpg">
@@ -190,6 +190,76 @@
             </div>
 
             
+        </div>
+        <div class="toutpromo2">
+        <div class="item_promo">
+                
+        <div class="image_produit"><img src="/image/cigarette-electronique-80w-complet-kit-kit-vape-av.jpg">
+                </div>
+                <div class="tout">
+                    <div class="nom_produit">
+                        <p>cigarette</p>
+                    </div>
+                    <div class="descprition_produit">
+                        <p>elle fume</p>
+                    </div>
+                    <div class="prix_produit">
+                        <p>10€</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="item_promo">
+                <div class="image_produit"><img src="/image/heisenberg.jpg">
+                </div>
+                <div class="tout">
+                    <div class="nom_produit">
+                        <p>Produit</p>
+                    </div>
+                    <div class="descprition_produit">
+                        <p>c'est liquide</p>
+                    </div>
+                    <div class="prix_produit">
+                        <p>5€</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="item_promo">
+                <div class="image_produit"><img src="/image/ecigarette.jpg">
+                </div>
+                <div class="tout">
+                    <div class="nom_produit">
+                        <p>ecigarette superturbo</p>
+                    </div>
+                    <div class="descprition_produit">
+                        <p>mega cigarette de la mort qui tue</p>
+                    </div>
+                    <div class="prix_produit">
+                        <p>un rein</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item_promo">
+                <div class="image_produit"><img src="/image/resistance-melo-ec-eleaf-a-l-unite-cigarette-electronique.jpg">
+                </div>
+                <div class="tout">
+                    <div class="nom_produit">
+                        <p>Resistance</p>
+                    </div>
+                    <div class="descprition_produit">
+                        <p>Resiste prouve que tu existes</p>
+                    </div>
+                    <div class="prix_produit">
+                        <p>24€</p>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
         </div>
         <H1>Nos Categorie !</H1>
