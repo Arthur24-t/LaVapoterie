97fc032430229e52b15fca161113f938259c3030
arthur <arthur.trus1@gmail.com>
2021-12-25 19:05:06 +0100

    g

--- Bryan/fonction-commande.php
+++ Bryan/fonction-commande.php
@@ -1,20 +1,15 @@
-
-
-
 <?php
 session_start();
 include_once("fonction-panier.php");
 
 
 try {
-//test de la connection
+    //test de la connection
     $pdo = new PDO(
         'mysql:host=localhost;dbname=site;charset=utf8',
         'root',
         '1234'
     );
-
-
 } catch (PDOException $exception) {
 
     mail('VOTRE_EMAIL', 'PDOException', $exception->getMessage());
@@ -23,20 +18,20 @@ try {
 
 
 
-$nbArticles = count($_SESSION['panier']['libelleProduit']);//recupere le nombre de produit dans la panier 
+$nbArticles = count($_SESSION['panier']['libelleProduit']); //recupere le nombre de produit dans la panier 
 
 for ($i = 0; $i < $nbArticles; $i++) {
-   $id= $_SESSION['panier']['idProduit'][$i];
-   $nb = $_SESSION['panier']['qteProduit'][$i];
-   
+    $id = $_SESSION['panier']['idProduit'][$i];
+    $nb = $_SESSION['panier']['qteProduit'][$i];
 
-//$requete = "SELECT prodStock FROM produit WHERE prodId = '$id'";
 
-$requete = "UPDATE produit SET prodStock= prodStock-$nb  WHERE prodID='$id'";
+    //$requete = "SELECT prodStock FROM produit WHERE prodId = '$id'";
 
-$exe=$pdo->prepare($requete);
-$exe->execute();
-$fini = $exe->fetch();
+    $requete = "UPDATE produit SET prodStock= prodStock-$nb  WHERE prodID='$id'";
+
+    $exe = $pdo->prepare($requete);
+    $exe->execute();
+    $fini = $exe->fetch();
 }
 
 supprimePanier();
@@ -46,22 +41,28 @@ supprimePanier();
 session_start();
 echo '<?xml version="1.0" encoding="utf-8"?>';
 ?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <meta charset="utf-8" />
+
 <head>
-<link rel="stylesheet" type="text/css" href="commande.css" />
+    <link rel="stylesheet" type="text/css" href="commande.css" />
 </head>
 <div class="head">
-<?php include("header.php"); ?>
+    <?php include("header.php"); ?>
 </div>
+
 
 <body>
     <div class="bordereaux">
-     <p>Merci de votre commande !</p> 
-     <p>vos produit sera expedié dans les meilleurs delais </p>
-     <a href="index.php">Poursuivez votre shopping</a>
-     </div>
+        <p>Merci de votre commande !</p>
+        <p>vos produit sera expedié dans les meilleurs delais </p>
+        <a href="index.php">Poursuivez votre shopping</a>
+    </div>
 </body>
 
 </html>
