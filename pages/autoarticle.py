1afe68f9ff9badd67df4ea09028a85dd8896d615
arthur <arthur.trus1@gmail.com>
2022-03-05 14:15:41 +0100

    fin

--- pages/autoarticle.py
+++ pages/autoarticle.py
@@ -1,43 +1,45 @@
 import shutil
-
 from sqlalchemy import desc;  # folder management
 
-numProduit = input("num prod\n");
-fichier = numProduit+".php";
-print(fichier)
-nomProduit= input("nom produit\n")
-description = input("description\n");
-prix = input("prix\n")
-prix += "€"
-bouton ="<a href='"+numProduit+".php?action=ajout&amp;i="+ numProduit +"&amp;l="+nomProduit+"&amp;q=1&amp;p="+prix+"'>Ajouter au panier</a></div>"
 
-image = "<img src='../image/produit/"+numProduit+".jpg'>"
+for i in range(0,30):
+    print("produit numero : "+ str(i))
+    numProduit = input("num prod\n");
+    fichier = numProduit+".php";
+    print(fichier)
+    nomProduit= input("nom produit\n")
+    description = input("description\n");
+    prix = input("prix\n")
+    prix += "€"
+    bouton ="<a href='"+numProduit+".php?action=ajout&amp;i="+ numProduit +"&amp;l="+nomProduit+"&amp;q=1&amp;p="+prix+"'>Ajouter au panier</a></div>"
 
-shutil.copy('produit.php',  fichier)
+    image = "<img src='../image/produit/"+numProduit+".jpg'>"
 
-print("le nom du nouveau fichier est :" + fichier)
+    shutil.copy('produit.php',  fichier)
 
-
-
-f = open(fichier, "r+", encoding='utf-8')
-
-f.seek(2170, 0)
+    print("le nom du nouveau fichier est :" + fichier)
 
 
-f.write(str(nomProduit))
 
-f.seek(2344, 0)
-f.write(str(image))
+    f = open(fichier, "r+", encoding='utf-8')
 
-f.seek(2509, 0)
+    f.seek(2170, 0)
 
-f.write(str(description))
-f.seek(2820, 0)
 
-f.write(str(prix))
-f.seek(2913, 0)
+    f.write(str(nomProduit))
 
-f.write(str(bouton))
+    f.seek(2344, 0)
+    f.write(str(image))
+
+    f.seek(2509, 0)
+
+    f.write(str(description))
+    f.seek(2820, 0)
+
+    f.write(str(prix))
+    f.seek(2913, 0)
+
+    f.write(str(bouton))
 
 
 
