ebc3c45ffb2afb080c16d88ef868dfeefefaad2a
PaulineWdi <pauline.wandolski@gmail.com>
2022-03-09 10:28:18 +0100

    H

--- pages/accessoire.php
+++ pages/accessoire.php
@@ -7,51 +7,51 @@ $erreur = false;
 
 $action = (isset($_POST['action']) ? $_POST['action'] : (isset($_GET['action']) ? $_GET['action'] : null));
 if ($action !== null) {
-   if (!in_array($action, array('ajout', 'suppression', 'refresh')))
-      $erreur = true;
+    if (!in_array($action, array('ajout', 'suppression', 'refresh')))
+        $erreur = true;
 
-   //récupération des variables en POST ou GET
-   $i = (isset($_POST['i']) ? $_POST['i'] : (isset($_GET['i']) ? $_GET['i'] : null));
-   $l = (isset($_POST['l']) ? $_POST['l'] : (isset($_GET['l']) ? $_GET['l'] : null));
-   $p = (isset($_POST['p']) ? $_POST['p'] : (isset($_GET['p']) ? $_GET['p'] : null));
-   $q = (isset($_POST['q']) ? $_POST['q'] : (isset($_GET['q']) ? $_GET['q'] : null));
+    //récupération des variables en POST ou GET
+    $i = (isset($_POST['i']) ? $_POST['i'] : (isset($_GET['i']) ? $_GET['i'] : null));
+    $l = (isset($_POST['l']) ? $_POST['l'] : (isset($_GET['l']) ? $_GET['l'] : null));
+    $p = (isset($_POST['p']) ? $_POST['p'] : (isset($_GET['p']) ? $_GET['p'] : null));
+    $q = (isset($_POST['q']) ? $_POST['q'] : (isset($_GET['q']) ? $_GET['q'] : null));
 
-   //Suppression des espaces verticaux
-   $l = preg_replace('#\v#', '', $l);
-   $i = preg_replace('#\v#', '', $i);
-   //On vérifie que $p est un float
-   $p = floatval($p);
+    //Suppression des espaces verticaux
+    $l = preg_replace('#\v#', '', $l);
+    $i = preg_replace('#\v#', '', $i);
+    //On vérifie que $p est un float
+    $p = floatval($p);
 
-   //On traite $q qui peut être un entier simple ou un tableau d'entiers
+    //On traite $q qui peut être un entier simple ou un tableau d'entiers
 
-   if (is_array($q)) {
-      $QteArticle = array();
-      $i = 0;
-      foreach ($q as $contenu) {
-         $QteArticle[$i++] = intval($contenu);
-      }
-   } else
-      $q = intval($q);
+    if (is_array($q)) {
+        $QteArticle = array();
+        $i = 0;
+        foreach ($q as $contenu) {
+            $QteArticle[$i++] = intval($contenu);
+        }
+    } else
+        $q = intval($q);
 }
 
 if (!$erreur) {
-   switch ($action) {
-      case "ajout":
-         ajouterArticle($i, $l, $q, $p);
-         break;
+    switch ($action) {
+        case "ajout":
+            ajouterArticle($i, $l, $q, $p);
+            break;
 
-      case "suppression":
-         supprimerArticle($l);
-         break;
+        case "suppression":
+            supprimerArticle($l);
+            break;
 
-      case "refresh":
-         for ($b = 0; $b < count($QteArticle); $b++) {
-            modifierQTeArticle($_SESSION['panier']['libelleProduit'][$b], round($QteArticle[$b]));
-         }
-         break;
-      default:
-         break;
-   }
+        case "refresh":
+            for ($b = 0; $b < count($QteArticle); $b++) {
+                modifierQTeArticle($_SESSION['panier']['libelleProduit'][$b], round($QteArticle[$b]));
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
@@ -68,10 +68,10 @@ echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 </head>
 
 <body>
-<div class="head">
+    <div class="head">
+        <?php include("header.php"); ?>
+    </div>
     <?php include("header.php"); ?>
-</div>
-<?php include("header.php"); ?>
 
     <div class="corps">
 
@@ -79,179 +79,197 @@ echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
         <div class="produit">
 
             <div class="item">
-                <div class="image_produit"><img src="../image/produit/A28.jpg">
-                </div>
-                <div class="tout">
-                    <div class="nom_produit">
-                        <p>Rangement </p>
+                <a href="./A28.php">
+                    <div class="image_produit"><img src="../image/produit/A28.jpg">
                     </div>
-                    <div class="descprition_produit">
-                        <p>Boite de rangement <br> pour cigarette</p>
+                    <div class="tout">
+                        <div class="nom_produit">
+                            <p>Rangement </p>
+                        </div>
+                        <div class="descprition_produit">
+                            <p>Boite de rangement <br> pour cigarette</p>
+                        </div>
+                        <div class="prix_produit">
+                            <p>25€</p>
+                        </div>
+                        <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A28&amp; l=Rangement&amp;q=1&amp;p=25">Ajouter au panier</a></div>
                     </div>
-                    <div class="prix_produit">
-                        <p>25€</p>
-                    </div>
-                    <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A28&amp; l=Rangement&amp;q=1&amp;p=25">Ajouter au panier</a></div>
-                </div>
+                </a>
             </div>
 
 
 
             <div class="item">
-                <div class="image_produit"><img src="../image/produit/A45.jpg">
-                </div>
-                <div class="tout">
-                    <div class="nom_produit">
-                        <p>Seringue </p>
+                <a href="/A45.php">
+                    <div class="image_produit"><img src="../image/produit/A45.jpg">
                     </div>
-                    <div class="descprition_produit">
-                        <p>Seringue de 10ml </p>
+                    <div class="tout">
+                        <div class="nom_produit">
+                            <p>Seringue </p>
+                        </div>
+                        <div class="descprition_produit">
+                            <p>Seringue de 10ml </p>
+                        </div>
+                        <div class="prix_produit">
+                            <p>2€</p>
+                        </div>
+                        <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A45&amp; l=Seringue&amp;q=1&amp;p=2">Ajouter au panier</a></div>
                     </div>
-                    <div class="prix_produit">
-                        <p>2€</p>
-                    </div>
-                    <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A45&amp; l=Seringue&amp;q=1&amp;p=2">Ajouter au panier</a></div>
-                </div>
+                </a>
             </div>
 
 
 
             <div class="item">
-                <div class="image_produit"><img src="../image/produit/A20.jpg">
-                </div>
-                <div class="tout">
-                    <div class="nom_produit">
-                        <p>Flacon</p>
+                <a href="/A20.php">
+                    <div class="image_produit"><img src="../image/produit/A20.jpg">
                     </div>
-                    <div class="descprition_produit">
-                        <p>Flacon pour liquide en plastique de contenance 100ml</p>
-                    </div>
-                    <div class="prix_produit">
-                        <p>5€</p>
-                    </div>
-                    <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A20&amp; l=Flacon&amp;q=1&amp;p=5">Ajouter au panier</a></div>
+                    <div class="tout">
+                        <div class="nom_produit">
+                            <p>Flacon</p>
+                        </div>
+                        <div class="descprition_produit">
+                            <p>Flacon pour liquide en plastique de contenance 100ml</p>
+                        </div>
+                        <div class="prix_produit">
+                            <p>5€</p>
+                        </div>
+                        <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A20&amp; l=Flacon&amp;q=1&amp;p=5">Ajouter au panier</a></div>
 
-                </div>
+                    </div>
+                </a>
             </div>
 
 
 
 
             <div class="item">
-                <div class="image_produit"><img src="../image/produit/A26.jpg">
-                </div>
-                <div class="tout">
-                    <div class="nom_produit">
-                        <p>Chargeur</p>
+                <a href="/A26.php">
+                    <div class="image_produit"><img src="../image/produit/A26.jpg">
                     </div>
-                    <div class="descprition_produit">
-                        <p>Chargeur pour cigarette X</p>
+                    <div class="tout">
+                        <div class="nom_produit">
+                            <p>Chargeur</p>
+                        </div>
+                        <div class="descprition_produit">
+                            <p>Chargeur pour cigarette X</p>
+                        </div>
+                        <div class="prix_produit">
+                            <p>140€</p>
+                        </div>
+                        <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A26&amp; l=Chargeur&amp;q=1&amp;p=140">Ajouter au panier</a></div>
                     </div>
-                    <div class="prix_produit">
-                        <p>140€</p>
-                    </div>
-                    <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A26&amp; l=Chargeur&amp;q=1&amp;p=140">Ajouter au panier</a></div>
-                </div>
+                </a>
             </div>
 
 
             <div class="item">
-                <div class="image_produit"><img src="../image/produit/A20.jpg">
-                </div>
-                <div class="tout">
-                    <div class="nom_produit">
-                        <p>Flacon</p>
+                <a href="/A20.php">
+                    <div class="image_produit"><img src="../image/produit/A20.jpg">
                     </div>
-                    <div class="descprition_produit">
-                        <p>Flacon pour liquide <br> en plastique de <br> contenance 100ml</p>
+                    <div class="tout">
+                        <div class="nom_produit">
+                            <p>Flacon</p>
+                        </div>
+                        <div class="descprition_produit">
+                            <p>Flacon pour liquide <br> en plastique de <br> contenance 100ml</p>
+                        </div>
+                        <div class="prix_produit">
+                            <p>5€</p>
+                        </div>
+                        <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A20&amp; l=Flacon&amp;q=1&amp;p=5">Ajouter au panier</a></div>
                     </div>
-                    <div class="prix_produit">
-                        <p>5€</p>
-                    </div>
-                    <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A20&amp; l=Flacon&amp;q=1&amp;p=5">Ajouter au panier</a></div>
-                </div>
-            </div>
-
-
-
-            <div class="item">
-                <div class="image_produit"><img src="../image/produit/A23.jpg">
-                </div>
-                <div class="tout">
-                    <div class="nom_produit">
-                        <p>Drip tip</p>
-                    </div>
-                    <div class="descprition_produit">
-                        <p>Drip tip Bleu</p>
-                    </div>
-                    <div class="prix_produit">
-                        <p>30€</p>
-                    </div>
-                    <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A23&amp; l=Drip Tip&amp;q=1&amp;p=30">Ajouter au panier</a></div>
-                </div>
-            </div>
-
-
-            <div class="item">
-                <div class="image_produit"><img src="../image/produit/A45.jpg">
-                </div>
-                <div class="tout">
-                    <div class="nom_produit">
-                        <p>Seringue </p>
-                    </div>
-                    <div class="descprition_produit">
-                        <p>Seringue de 10ml </p>
-                    </div>
-                    <div class="prix_produit">
-                        <p>2€</p>
-                    </div>
-                    <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A45&amp; l=Seringue&amp;q=1&amp;p=2">Ajouter au panier</a></div>
-                </div>
+                </a>
             </div>
 
 
 
             <div class="item">
-                <div class="image_produit"><img src="../image/produit/A45.jpg">
-                </div>
-                <div class="tout">
-                    <div class="nom_produit">
-                        <p>Seringue </p>
+                <a href="/A23.php">
+                    <div class="image_produit"><img src="../image/produit/A23.jpg">
                     </div>
-                    <div class="descprition_produit">
-                        <p>Seringue de 10ml </p>
+                    <div class="tout">
+                        <div class="nom_produit">
+                            <p>Drip tip</p>
+                        </div>
+                        <div class="descprition_produit">
+                            <p>Drip tip Bleu</p>
+                        </div>
+                        <div class="prix_produit">
+                            <p>30€</p>
+                        </div>
+                        <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A23&amp; l=Drip Tip&amp;q=1&amp;p=30">Ajouter au panier</a></div>
                     </div>
-                    <div class="prix_produit">
-                        <p>2€</p>
-                    </div>
-                    <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A45&amp; l=Seringue&amp;q=1&amp;p=2">Ajouter au panier</a></div>
-                </div>
+                </a>
             </div>
 
-          
 
             <div class="item">
-                <div class="image_produit"><img src="../image/produit/A45.jpg">
-                </div>
-                <div class="tout">
-                    <div class="nom_produit">
-                        <p>Seringue </p>
+                <a href="A45.php">
+                    <div class="image_produit"><img src="../image/produit/A45.jpg">
                     </div>
-                    <div class="descprition_produit">
-                        <p>Seringue de 10ml </p>
+                    <div class="tout">
+                        <div class="nom_produit">
+                            <p>Seringue </p>
+                        </div>
+                        <div class="descprition_produit">
+                            <p>Seringue de 10ml </p>
+                        </div>
+                        <div class="prix_produit">
+                            <p>2€</p>
+                        </div>
+                        <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A45&amp; l=Seringue&amp;q=1&amp;p=2">Ajouter au panier</a></div>
                     </div>
-                    <div class="prix_produit">
-                        <p>2€</p>
+                </a>
+            </div>
+
+
+
+            <div class="item">
+                <a href="/A45.php">
+                    <div class="image_produit"><img src="../image/produit/A45.jpg">
                     </div>
-                    <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A45&amp; l=Seringue&amp;q=1&amp;p=2">Ajouter au panier</a></div>
-                </div>
+                    <div class="tout">
+                        <div class="nom_produit">
+                            <p>Seringue </p>
+                        </div>
+                        <div class="descprition_produit">
+                            <p>Seringue de 10ml </p>
+                        </div>
+                        <div class="prix_produit">
+                            <p>2€</p>
+                        </div>
+                        <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A45&amp; l=Seringue&amp;q=1&amp;p=2">Ajouter au panier</a></div>
+                    </div>
+                </a>
+            </div>
+
+
+
+            <div class="item">
+                <a href="/A45.php">
+                    <div class="image_produit"><img src="../image/produit/A45.jpg">
+                    </div>
+                    <div class="tout">
+                        <div class="nom_produit">
+                            <p>Seringue </p>
+                        </div>
+                        <div class="descprition_produit">
+                            <p>Seringue de 10ml </p>
+                        </div>
+                        <div class="prix_produit">
+                            <p>2€</p>
+                        </div>
+                        <div class="bouton"><a href="accessoire.php?action=ajout&amp;i=A45&amp; l=Seringue&amp;q=1&amp;p=2">Ajouter au panier</a></div>
+                    </div>
+                </a>
             </div>
 
 
 
 
         </div>
-      
+
 </body>
 
 </html>
